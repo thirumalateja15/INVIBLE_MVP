89947876386d07c38fb881377f004d8655aaae03
DODDA123SOWMYA <thirumlateja03@gmail.com>
2023-08-16 14:52:16 +0530

    version1

---
+++ app/src/main/java/com/example/inviblempv/starter.java
@@ -0,0 +1,46 @@
+package com.example.inviblempv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.VideoView;
+
+public class starter extends AppCompatActivity {
+
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(1);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        setContentView(R.layout.activity_starter);
+
+        videoView = findViewById(R.id.start_vedio);
+
+        String path = "android.resource://com.example.inviblempv/" + R.raw.starter;
+
+        Uri uri = Uri.parse(path);
+        videoView.setVideoURI(uri);
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+            }
+        });
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Intent intent = new Intent(starter.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
